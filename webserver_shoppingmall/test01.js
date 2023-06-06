4234969b08a6fa09cf97d779eefe281d8eb2de97
kekemon <am20190954@gmail.com>
2023-06-06 16:08:47 +0900

    go

--- webserver_shoppingmall/test01.js
+++ webserver_shoppingmall/test01.js
@@ -208,6 +208,6 @@ app.get('/register', (req, res) => {
   res.render('register');
 });
 
-app.listen(3000, '192.168.219.100', function () {
-  console.log('http://116.42.144.101:3000/ app listening on port 3000!');
+app.listen(3000, '192.168.35.120', function () {
+  console.log('http://192.168.35.120:3000/ app listening on port 3000!');
 });
