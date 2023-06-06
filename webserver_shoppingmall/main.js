19011ac8abd084140df76ef0cd5184a9b0a8f732
kekemon <am20190954@gmail.com>
2023-06-07 01:56:07 +0900

    real last commit

--- webserver_shoppingmall/main.js
+++ webserver_shoppingmall/main.js
@@ -6,13 +6,21 @@ const url = require('url');
 const qs = require('querystring');
 const crypto = require('crypto');
 const mysql = require('mysql');
-const {saveItemsToDatabase, saveUserToDatabase, getUserFromDatabase, saveCommentToDatabase, getCommentsFromDatabase, saveRatingToDatabase, getRatingFromDatabase} = require(
-    './database'
-);
+const {
+    saveItemsToDatabase,
+    saveUserToDatabase,
+    getUserFromDatabase,
+    saveCommentToDatabase,
+    getCommentsFromDatabase,
+    saveRatingToDatabase,
+    getRatingFromDatabase
+} = require('./database');
 const sys = require('./system');
+
 const client_id = 'uR0FsbWPkbkFc2AFaUwy';
 const client_secret = 'PUq6k8Cvip';
 
+// 쿠키 파싱 함수
 const parseCookies = (cookie = '') => cookie
     .split(';')
     .map(v => v.split('='))
@@ -21,20 +29,21 @@ const parseCookies = (cookie = '') => cookie
         return acc;
     }, {});
 
-const session = {};
-const users = {};
-var onSearch;
-var user_ID;
+const session = {}; // 세션 데이터를 저장할 객체
+const users = {}; // 사용자 데이터를 저장할 객체
+var onSearch; // 검색어를 저장할 변수
+var user_ID; // 사용자 ID를 저장할 변수
 app.set('view engine', 'pug');
 
 const connection = mysql.createConnection({
-    host: 'localhost', // centos 서버 아이피
-    user: 'root', // oracle12
-    password: 'Mysql123!@#', // Mysql123!@#
-    database: 'nodejs_shoppingmall' // nodejs_shoppingmall
+    host: 'localhost', // 데이터베이스 호스트
+    user: 'root', // 데이터베이스 사용자 이름
+    password: 'root', // 데이터베이스 비밀번호
+    database: 'nodejs_shoppingmall' // 데이터베이스 이름
 });
-connection.connect();
+connection.connect(); // 데이터베이스 연결
 
+// 메인 페이지
 app.get('/main', function (req, res) {
     console.log("IP: " + req.ip + " / 접속");
     const cookies = parseCookies(req.headers.cookie);
@@ -52,8 +61,9 @@ app.get('/main', function (req, res) {
     });
 });
 
+// 검색 페이지
 app.get('/search', function (req, res) {
-    onSearch = req.query.query;
+    onSearch = req.query.query; // 검색어를 저장
     const api_url = 'https://openapi.naver.com/v1/search/shop.json?query=' +
         encodeURI(onSearch) + '&display=50';
     const options = {
@@ -145,7 +155,7 @@ app.get('/search', function (req, res) {
         });
 });
 
-
+// 회원 가입 처리
 app.post('/register', (req, res) => {
     const body = [];
     req
@@ -199,35 +209,35 @@ app.post('/register', (req, res) => {
         });
 });
 
-// 댓글 추가
+// 댓글 추가 처리
 app.get('/search/comment', (req, res) => {
-  const item = req.query.itemName; // 상품 이름을 파라미터로 받음
-  const comment = req.query.comment; // 클라이언트에서 전송된 댓글 내용
-  const commenter = user_ID;
-  // 댓글을 데이터베이스 또는 다른 저장소에 추가
-  saveCommentToDatabase(item, comment, commenter); // 데이터베이스에 댓글 저장하는 함수 호출
+    const item = req.query.itemName; // 상품 이름을 파라미터로 받음
+    const comment = req.query.comment; // 클라이언트에서 전송된 댓글 내용
+    const commenter = user_ID;
+    // 댓글을 데이터베이스 또는 다른 저장소에 추가
+    saveCommentToDatabase(item, comment, commenter); // 데이터베이스에 댓글 저장하는 함수 호출
 
-  // 댓글 목록을 클라이언트로 전송
-  const redirectURL = '/search?query=' + encodeURIComponent(onSearch);
-  res.redirect(redirectURL);
+    // 댓글 목록을 클라이언트로 전송
+    const redirectURL = '/search?query=' + encodeURIComponent(onSearch);
+    res.redirect(redirectURL);
 });
 
-// 평점 추가
+// 평점 추가 처리
 app.get('/search/rating', (req, res) => {
-  const item = req.query.itemName; // 상품 이름을 파라미터로 받음
-  const rating = req.query.rating; // 클라이언트에서 전송된 댓글 내용
-  // 평점을을 데이터베이스 또는 다른 저장소에 추가
-  saveRatingToDatabase(item, rating); // 데이터베이스에 댓글 저장하는 함수 호출
+    const item = req.query.itemName; // 상품 이름을 파라미터로 받음
+    const rating = req.query.rating; // 클라이언트에서 전송된 평점
+    // 평점을 데이터베이스 또는 다른 저장소에 추가
+    saveRatingToDatabase(item, rating); // 데이터베이스에 평점 저장하는 함수 호출
 
-  // 새로 추가된 평점을 포함한 전체 댓글 목록을 조회
-  const ratings = getRatingFromDatabase(item); // 데이터베이스에서 댓글 조회하는 함수 호출
+    // 새로 추가된 평점을 포함한 전체 댓글 목록을 조회
+    const ratings = getRatingFromDatabase(item); // 데이터베이스에서 댓글 조회하는 함수 호출
 
-  // 댓글 목록을 클라이언트로 전송
-  const redirectURL = '/search?query=' + encodeURIComponent(onSearch);
-  res.redirect(redirectURL);
+    // 댓글 목록을 클라이언트로 전송
+    const redirectURL = '/search?query=' + encodeURIComponent(onSearch);
+    res.redirect(redirectURL);
 });
 
-
+// 로그인 처리
 app.post('/login', (req, res) => {
     const body = [];
     req
@@ -282,6 +292,7 @@ app.post('/login', (req, res) => {
         });
 });
 
+// 진입 페이지 처리
 app.get('/', (req, res) => {
     const cookies = parseCookies(req.headers.cookie);
     const sessionId = cookies.session;
@@ -290,14 +301,16 @@ app.get('/', (req, res) => {
     res.render('entry');
 });
 
+// 로그인 페이지 처리
 app.get('/login', (req, res) => {
     res.render('login');
 });
 
+// 회원 가입 페이지 처리
 app.get('/register', (req, res) => {
     res.render('register');
 });
 
-app.listen(3000, '192.168.35.120', function () {
+app.listen(3000, '192.168.219.100', function () {
     console.log('http://192.168.35.120:3000/ app listening on port 3000!');
 });
